26bac285d74e00a140974ef727af32527460744c
adonisbeltran <ad.beltran@duocuc.cl>
2026-05-01 14:57:39 -0400

    Se agrega sistema

---
+++ src/main/java/com/gestiondeportiva/SistemaGestionDeportivaApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondeportiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionDeportivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionDeportivaApplication.class, args);
+	}
+
+}
