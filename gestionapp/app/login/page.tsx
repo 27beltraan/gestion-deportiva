f2e70e34ea02a476ea224aae4281f90c5eed8551
adonisbeltran <ad.beltran@duocuc.cl>
2026-05-04 03:09:49 -0400

    se implementa el home, se crea el diseno principal y de se definen colores principales

--- gestionapp/app/login/page.tsx
+++ gestionapp/app/login/page.tsx
@@ -1,54 +1,112 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { supabase } from '@/lib/supabase'
-import { useRouter } from 'next/navigation'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
+
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
+  const router = useRouter();
 
-  const handleLogin = async () => {
-    console.log('Intentando login...') // 👈 debug
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-
-    if (error) {
-      alert(error.message)
-      return
+  const handleLogin = () => {
+    // cone subase
+    if (email && password) {
+      router.push("/dashboard");
+    } else {
+      alert("Completa los campos");
     }
-
-   
-    router.push('/dashboard')
-  }
+  };
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Login</h1>
+        
+    <div className="h-screen flex">
 
-      <input
-        type="email"
-        placeholder="Correo"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      {/* formulari izq */}
+      <div className="w-full md:w-1/2 flex flex-col justify-center px-10">
 
-      <br /><br />
+        {/* logo */}
+        <img
+        src="/logosecundario.png"
+        alt="logo"
+        className="h-50 w-auto object-contain"
+/>
 
-      <input
-        type="password"
-        placeholder="Contraseña"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+<div className="absolute top-6 left-6">
+  <button
+    onClick={() => router.push("/")}
+    className="bg-gray-100 p-2 rounded-full hover:bg-purple-500 hover:text-white transition"
+  >
+    <ArrowLeft size={20} />
+  </button>
+</div>
 
-      <br /><br />
+        <h1 className="text-3xl font-bold mb-2">
+          ¿Listo para comenzar?
+        </h1>
 
-      <button type="button" onClick={handleLogin}>
-        Iniciar sesión
-      </button>
+        <p className="text-gray-500 mb-6">
+          Ingresa tus datos para acceder a tu cuenta
+        </p>
+
+        {/* email */}
+        <input
+          type="email"
+          placeholder="Ingresa tu email"
+          className="border rounded-lg p-3 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        {/* contra*/}
+        <input
+          type="password"
+          placeholder="Contraseña"
+          className="border rounded-lg p-3 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          onClick={handleLogin}
+          className="bg-purple-500 text-white py-3 rounded-lg hover:bg-purple-600 transition"
+        >
+          Ingresar
+        </button>
+
+        <p className="mt-4 text-sm">
+          ¿No tienes cuenta?{" "}
+          <a href="/register" className="text-purple-500 font-semibold">
+            Regístrate
+          </a>
+        </p>
+      </div>
+
+      {/* imagen derecha) */}
+      <div className="hidden md:block w-1/2 relative">
+
+        <img
+          src="/fotoiniciarsesion.jpg"
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute inset-0 bg-gradient-to-br from-black-600/90 to-pink-500/50"></div>
+
+        <div className="absolute top-10 left-10 text-white max-w-xs">
+          <h2 className="text-2xl font-bold">
+            Controla tu club de forma inteligente
+          </h2>
+        </div>
+
+
+
+      </div>
     </div>
-  )
+
+
+
+  );
 }
+
--- gestionapp/app/page.tsx
+++ gestionapp/app/page.tsx
@@ -1,65 +1,51 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="h-screen w-full relative text-white">
+
+      {/* fondoprincipal */}
+      <div className="absolute inset-0">
+        <img
+          src="/portada.png"
+          className="w-full h-full object-cover"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-purple-900/60 to-transparent"></div>
+      </div>
+
+      {/* navbar */}
+      <div className="relative z-10 flex justify-between items-center p-6">
+        <img src="/logoprincipal.PNG" alt="logo" className="h-30" />
+
+        <div className="flex gap-4">
+          <a href="/login" className="px-4 py-2 bg-white text-black rounded-lg">
+            Iniciar Sesión
+          </a>
+          <a href="/register" className="px-4 py-2 border border-white rounded-lg">
+            Crear Cuenta
+          </a>
+        </div>
+      </div>
+
+      {/* texto */}
+      <div className="relative z-10 flex h-[80%] items-center px-10">
+
+        <div className="max-w-xl">
+          <h1 className="text-6xl font-bold leading-tight">
+            Gestión Inteligente <br />
+            para tu <span className="text-purple-400">Club.</span>
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+
+          <p className="mt-6 text-gray-300 text-lg">
+            Automatiza procesos y optimiza la experiencia de tus socios con tecnología moderna.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+
+          <a href="/register">
+            <button className="mt-6 px-6 py-3 bg-purple-500 rounded-lg hover:bg-purple-600 transition">
+              ver
+            </button>
           </a>
         </div>
-      </main>
+
+      </div>
     </div>
-  );
+  )
 }
